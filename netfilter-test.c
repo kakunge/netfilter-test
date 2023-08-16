d9a67cf739c6bae5d932b9ac29b1ec970767dc3a
kakunge <kakunge3@naver.com>
2023-08-17 04:11:10 +0900

    Update netfilter-test.c

--- netfilter-test.c
+++ netfilter-test.c
@@ -22,61 +22,6 @@ void dump(unsigned char* buf, int size) {
 	printf("\n");
 }
 
-static u_int32_t print_pkt (struct nfq_data *tb) {
-	int id = 0;
-	struct nfqnl_msg_packet_hdr *ph;
-	struct nfqnl_msg_packet_hw *hwph;
-	u_int32_t mark,ifi;
-	int ret;
-	unsigned char *data;
-
-	ph = nfq_get_msg_packet_hdr(tb);
-	if (ph) {
-		id = ntohl(ph->packet_id);
-		printf("hw_protocol=0x%04x hook=%u id=%u ", ntohs(ph->hw_protocol), ph->hook, id);
-	}
-
-	hwph = nfq_get_packet_hw(tb);
-	if (hwph) {
-		int i, hlen = ntohs(hwph->hw_addrlen);
-
-		printf("hw_src_addr=");
-		for (i = 0; i < hlen-1; i++)
-			printf("%02x:", hwph->hw_addr[i]);
-		printf("%02x ", hwph->hw_addr[hlen-1]);
-	}
-
-	mark = nfq_get_nfmark(tb);
-	if (mark)
-		printf("mark=%u ", mark);
-
-	ifi = nfq_get_indev(tb);
-	if (ifi)
-		printf("indev=%u ", ifi);
-
-	ifi = nfq_get_outdev(tb);
-	if (ifi)
-		printf("outdev=%u ", ifi);
-	ifi = nfq_get_physindev(tb);
-	if (ifi)
-		printf("physindev=%u ", ifi);
-
-	ifi = nfq_get_physoutdev(tb);
-	if (ifi)
-		printf("physoutdev=%u ", ifi);
-
-	ret = nfq_get_payload(tb, &data);
-	if (ret >= 0) {
-		printf("payload_len=%d\n", ret);
-		dump(data, ret);
-		printf("\n");
-	}
-
-	fputc('\n', stdout);
-
-	return id;
-}
-
 static u_int32_t get_payload (struct nfq_data *tb) {
 	int id = 0;
 	struct nfqnl_msg_packet_hdr *ph;
@@ -131,12 +76,10 @@ static u_int32_t get_payload (struct nfq_data *tb) {
 		printf("\n");
 	}
 
-	printf("\n");
 	unsigned char ip[20];
 	for (int i = 0; i < 20; i++) {
 		ip[i] = data[i];
 	}
-	dump(ip, 20);
 
 	if (ip[9] != 0x06)
 		return id;
@@ -145,7 +88,6 @@ static u_int32_t get_payload (struct nfq_data *tb) {
 	for (int i = 0; i < 20; i++) {
 		tcp[i] = data[i+20];
 	}
-	dump(tcp, 20);
 
 	unsigned char tcpOffset = ((tcp[12] >> 4) * 4) - 20;
 
