259ef31255046797bd9782b9890cb056b77a4bfd
kakunge <kakunge3@naver.com>
2023-08-17 03:25:36 +0900

    Update netfilter-test.c

--- netfilter-test.c
+++ netfilter-test.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <linux/types.h>
-#include <linux/netfilter.h>		/* for NF_ACCEPT */
+#include <linux/netfilter.h>
 #include <errno.h>
 
 #include <libnetfilter_queue/libnetfilter_queue.h>
@@ -18,7 +18,6 @@ void dump(unsigned char* buf, int size) {
 	printf("\n");
 }
 
-/* returns packet id */
 static u_int32_t print_pkt (struct nfq_data *tb) {
 	int id = 0;
 	struct nfqnl_msg_packet_hdr *ph;
@@ -30,8 +29,7 @@ static u_int32_t print_pkt (struct nfq_data *tb) {
 	ph = nfq_get_msg_packet_hdr(tb);
 	if (ph) {
 		id = ntohl(ph->packet_id);
-		printf("hw_protocol=0x%04x hook=%u id=%u ",
-			ntohs(ph->hw_protocol), ph->hook, id);
+		printf("hw_protocol=0x%04x hook=%u id=%u ", ntohs(ph->hw_protocol), ph->hook, id);
 	}
 
 	hwph = nfq_get_packet_hw(tb);
@@ -75,8 +73,98 @@ static u_int32_t print_pkt (struct nfq_data *tb) {
 	return id;
 }
 
+static u_int32_t get_payload (struct nfq_data *tb) {
+	int id = 0;
+	struct nfqnl_msg_packet_hdr *ph;
+	struct nfqnl_msg_packet_hw *hwph;
+	u_int32_t mark,ifi;
+	int ret;
+	unsigned char *data;
+
+	ph = nfq_get_msg_packet_hdr(tb);
+	if (ph) {
+		id = ntohl(ph->packet_id);
+		printf("hw_protocol=0x%04x hook=%u id=%u ", ntohs(ph->hw_protocol), ph->hook, id);
+	}
+
+	hwph = nfq_get_packet_hw(tb);
+	if (hwph) {
+		int i, hlen = ntohs(hwph->hw_addrlen);
+
+		printf("hw_src_addr=");
+		for (i = 0; i < hlen-1; i++)
+			printf("%02x:", hwph->hw_addr[i]);
+		printf("%02x ", hwph->hw_addr[hlen-1]);
+	}
+
+	mark = nfq_get_nfmark(tb);
+	if (mark)
+		printf("mark=%u ", mark);
+
+	ifi = nfq_get_indev(tb);
+	if (ifi)
+		printf("indev=%u ", ifi);
+
+	ifi = nfq_get_outdev(tb);
+	if (ifi)
+		printf("outdev=%u ", ifi);
+	ifi = nfq_get_physindev(tb);
+	if (ifi)
+		printf("physindev=%u ", ifi);
+
+	ifi = nfq_get_physoutdev(tb);
+	if (ifi)
+		printf("physoutdev=%u ", ifi);
+
+	ret = nfq_get_payload(tb, &data);
+
+	if (ntohs(ph->hw_protocol) != 0x0800)
+		return id;
+
+	if (ret >= 0) {
+		printf("payload_len=%d\n", ret);
+		dump(data, ret);
+		printf("\n");
+	}
+
+	printf("\n");
+	unsigned char ip[20];
+	for (int i = 0; i < 20; i++) {
+		ip[i] = data[i];
+	}
+	dump(ip, 20);
+
+	if (ip[9] != 0x06)
+		return id;
+
+	unsigned char tcp[20];
+	for (int i = 0; i < 20; i++) {
+		tcp[i] = data[i+20];
+	}
+	dump(tcp, 20);
+	unsigned char tcpOffset = ((tcp[12] >> 4) * 4) - 20;
+
+	unsigned char* http;
+	http = (unsigned char*)malloc(sizeof(char) * (ret-20-20-tcpOffset));
+
+	for (int i = 0; i < ret; i++) {
+		http[i] = data[i+20+20+tcpOffset];
+	}
+	printf("123\n");
+	dump(http, ret-20-20-tcpOffset);
+
+
+	free(http);
+
+	fputc('\n', stdout);
+
+	return id;
+}
+
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data) {
-	u_int32_t id = print_pkt(nfa);
+	u_int32_t id = get_payload(nfa);
+
+
 	printf("entering callback\n");
 	return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
 }
@@ -91,6 +179,8 @@ int main(int argc, char **argv) {
 		usage();
 		return -1;
 	}
+
+	char* filterURL = argv[1];
 	
 	struct nfq_handle *h;
 	struct nfq_q_handle *qh;
@@ -139,13 +229,7 @@ int main(int argc, char **argv) {
 			nfq_handle_packet(h, buf, rv);
 			continue;
 		}
-		/* if your application is too slow to digest the packets that
-		 * are sent from kernel-space, the socket buffer that we use
-		 * to enqueue packets may fill up returning ENOBUFS. Depending
-		 * on your application, this error may be ignored. nfq_nlmsg_verdict_putPlease, see
-		 * the doxygen documentation of this library on how to improve
-		 * this situation.
-		 */
+		
 		if (rv < 0 && errno == ENOBUFS) {
 			printf("losing packets!\n");
 			continue;
@@ -158,8 +242,6 @@ int main(int argc, char **argv) {
 	nfq_destroy_queue(qh);
 
 #ifdef INSANE
-	/* normally, applications SHOULD NOT issue this command, since
-	 * it detaches other programs/sockets from AF_INET, too ! */
 	printf("unbinding from AF_INET\n");
 	nfq_unbind_pf(h, AF_INET);
 #endif
